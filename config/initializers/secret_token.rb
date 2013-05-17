f4308bff641078547952d3f112e035bdb3ea0f50
stephenalexbrowne <stephenalexbrowne@gmail.com>
2013-05-17 00:54:41 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brackets4lifeOffseason::Application.config.secret_token = '253ffdaf6752b4751112f95e392c1294d92a4c78cf2cd1a95fb66f13948f7d2b31e1f9cc843398011742ca138e8e51e921e41ff07937a089df19940947a2cdf0'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Brackets4lifeOffseason::Application.routes.draw do
+  
+  # route everything to the same page
+  root :to => 'home#index'
+
+end
